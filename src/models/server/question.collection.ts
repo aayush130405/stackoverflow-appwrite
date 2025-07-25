a3cacf5d930b52c1937821bede1f4994e7748fa4
Aayush <aayush130405@gmail.com>
2025-07-25 15:32:57 +0530

    add attrbutes and indexes in questioncollection

--- src/models/server/question.collection.ts
+++ src/models/server/question.collection.ts
@@ -1,4 +1,4 @@
-import {Permission} from "node-appwrite"
+import {IndexType, Permission} from "node-appwrite"
 
 import {db, questionCollection} from "../name"
 import {databases} from "./config"
@@ -13,4 +13,21 @@ export default async function createQuestionCollection() {
         Permission.delete("users")
     ])
     console.log("Question collection is created");
+
+    //create attributes
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+    ])
+    console.log("Question attributes created");
+
+    //create indexes
+    await Promise.all([
+        databases.createIndex(db, questionCollection, "title", IndexType.Fulltext, ["title"], ['asc']),
+        databases.createIndex(db, questionCollection, "content", IndexType.Fulltext, ["content"], ['asc'])
+
+    ])
 }
