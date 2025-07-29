bb772cf081bd5d983cfce746d8fe6adabc2fe80e
Aayush <aayush130405@gmail.com>
2025-07-29 22:38:11 +0530

    add delete answer route in answer api

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -43,8 +43,13 @@ export async function DELETE(request: NextRequest) {
         const response = await databases.deleteDocument(db, answerCollection, answerId)     //remove the answer
 
         //decrease the reputation
-        
+        const prefs = await users.getPrefs<UserPrefs>(answer.authorId)
 
+        await users.updatePrefs(answer.authorId, {
+            reputation: Number(prefs.reputation) - 1
+        })
+
+        return NextResponse.json({data: response}, {status: 200})
     } catch (error: any) {
         return NextResponse.json(
             {error: error?.message || "Error deleting answer"},
